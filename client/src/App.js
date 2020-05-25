3a760571b0d29a8d8445523b1a8050dd04551021
Darksice <darksice99@gmail.com>
2020-05-25 13:36:48 +0200

    Deploy takeoff

---
+++ client/src/App.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './App.css';
+import logoSite from './img/logo.png';
+import logoConnexion from './img/connexion.jpg';
+
+class App extends React.Component {
+
+  //const [merchants, setPays] = useState(false);
+  // useEffect(() => {
+  //   getPays();
+  // }, []);
+
+  constructor(props)
+  {
+    super(props);
+    this.state = { 
+      list: [],
+      tailleList:0
+    };
+  }
+
+  getPays() {
+    fetch('https://orbistory.herokuapp.com/api/pays')
+      .then(response => response.json())
+        .then(data => {
+          var joined = this.state.list.concat(data[0].pays);
+          this.setState({
+              list: joined,
+              tailleList: data.length
+            })
+        })
+    }
+
+  render(){
+    return (
+      <div className="App">
+        {this.getPays()}
+        {console.log("hffihzfhzfoihuzfhzo")}
+        {console.log(window.location.hostname)}
+        {console.log(window.location.protocol)}
+        <h1>{this.state.list[0]} </h1>
+        <header className="App-header">
+          <a href='/'><img className="logo" src={logoSite} alt="H"></img></a>
+          <img className="connexion" src={logoConnexion} alt="C"></img>
+        </header>
+
+        <h1> NOM DU SITE </h1>
+        
+        <div className="corps">
+          <div className="Pays">
+            <h2>Pays</h2>
+          </div>
+
+          <div className="Année">
+            <h2>Année</h2>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.get('/api/pays',cors(),async(req,res,next) => {
     }
 })
 
+
+const path = require('path')
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT,() =>{
     console.log(`App running on port ${PORT}.`)
